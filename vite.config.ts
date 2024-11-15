9acc7e36c9f91206a8a76e6f8ac2b800c2b884d1
apollo <763753335@qq.com>
2024-11-15 10:06:12 +0800

    配置信息

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/variables.scss";`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@layout': path.resolve(__dirname, 'src/layout'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+    },
+    // 支持文件扩展名自动补全
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 80,
+    strictPort: true,
+    open: true,
+    proxy: {
+      // 可以在这里配置代理
+    },
+  },
+});
